92c99e70cf25ecb68ed03f0eb1a02158e5a41e8c
Laura Andrade <laura.andradeweb@gmail.com>
2018-03-29 15:53:39 -0300

    Estrutura Form

---
+++ js/controlles/Cliente.js
@@ -0,0 +1,37 @@
+class Cliente {
+	constructor(nome,email,contratante) {
+		this._nome = nome;
+		this._email = email;
+		this._empresa = empresa;
+		this._contratante= contratante;
+		this._analise = analise;
+		this._lidar = lidar;
+		this._solucao = solucao;
+		Object.freeze(this);
+	}
+
+	get nome() {
+		return this._nome;
+	}
+
+	get email() {
+		return this.email;
+	}
+
+	get contratante() {
+		return this.contratante;
+	}
+
+	get analise() {
+		return this._analise;
+	}
+
+	get lidar() {
+		return this._lidar;
+	}
+
+	get solucao() {
+		return this._solucao;
+	}
+
+}
---
+++ js/controlles/ClienteController.js
@@ -0,0 +1,53 @@
+class ClienteController {
+
+	constructor(){
+		let $ = document.querySelector.bind(document);
+		this._inputNome = $("#nome");
+		this._inputEmail = $("#email");		
+		this._inputContratante = $("#contratante");	
+		this._inputAnalise = $('#FormControlTextarea1');
+		this._inputLidar = $('#FormControlTextarea2');
+		this._inputSolucao = $('#FormControlTextarea3');
+	}
+
+	adiciona(event) {
+		event.preventDefault();
+
+		let cliente = new Cliente(
+			this._inputNome.value,
+			this._inputEmail.value,
+			this._inputContratante.value,
+			this._inputAnalise.value,
+			this._inputLidar.value,
+			this._inputSolucao.value
+			);
+
+		// console.log(cliente);
+		if(cliente !== undefined) {
+			let cont = cliente.nome.split(" ");
+			if(cont.length < 2){
+				alert("Insira seu nome completo")
+			}else{
+				//converter JSON cliente em uma string
+				let stringCliente = JSON.stringify(cliente);
+
+				//salvar no local storage
+				window.localStorage.setItem('cliente', stringCliente);
+
+				//Resgatando o dado
+				let localCliente = window.localStorage.getItem('cliente', stringCliente);
+				console.log(localCliente);
+
+				//Construção div de sucesso
+				let div = document.getElementById("success");
+				let conteudo = document.createTextNode("Cadastro Realizado com Action!");
+				let elemento = document.createElement("div");
+				elemento.appendChild(conteudo);
+				elemento.setAttribute("class", "success");
+				div.appendChild(elemento); 	
+			}
+		}else {
+			console.log("erro");
+		}
+	}
+}
